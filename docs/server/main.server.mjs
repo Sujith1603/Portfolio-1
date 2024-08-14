703a447c4126e020d44c707966cd8f1ceb13f177
Sujith1603 <134761214+Sujith1603@users.noreply.github.com>
2024-08-14 13:36:27 +0530

    chavuu

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-TRJUCFBF.mjs";import"./chunk-GHJ2QZX3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MaterialModule } from './material/material.module';
+import { Header2Component } from './header2/header2.component';
+import { ServicesComponent } from './services/services.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { HistoryComponent } from './history/history.component';
+import { EndComponent } from './end/end.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { WorkComponent } from './work/work.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    Header2Component,
+    ServicesComponent,
+    ProjectsComponent,
+    HistoryComponent,
+    EndComponent,
+    AboutComponent,
+    ContactComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [
     provideClientHydration()
